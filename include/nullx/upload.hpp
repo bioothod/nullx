53c88201f15a2138b5ecbfda9f3665ad8460b691
Evgeniy Polyakov <zbr@ioremap.net>
2016-05-20 22:02:27 +0300

    upload: added auth check

--- include/nullx/upload.hpp
+++ include/nullx/upload.hpp
@@ -88,6 +88,21 @@ public:
 	virtual void on_request(const thevoid::http_request &req) {
 		m_timer.restart();
 
+		if (!this->server()->check_cookie(req, m_mbox)) {
+			NLOG_ERROR("upload: on_request: url: %s: invalid cookie, redirecting to login page",
+					req.url().to_human_readable().c_str());
+			thevoid::http_response reply;
+			reply.headers().set("Access-Control-Allow-Origin", "*");
+			reply.set_code(swarm::http_response::forbidden);
+			reply.headers().set_content_length(0);
+			this->send_reply(std::move(reply));
+			return;
+		}
+
+		NLOG_INFO("upload: on_request: url: %s: auth succeeded: username: %s, meta_bucket: %s, meta_index: %s",
+				req.url().to_human_readable().c_str(),
+				m_mbox.username.c_str(), m_mbox.meta_bucket.c_str(), m_mbox.meta_index.c_str());
+
 		this->set_chunk_size(10 * 1024 * 1024);
 
 		try {
@@ -183,6 +198,8 @@ protected:
 	uint64_t m_size;
 
 	ribosome::timer m_timer;
+
+	mailbox_t m_mbox;
 
 	virtual void on_write_partial(const elliptics::sync_write_result &result, const elliptics::error_info &error) {
 		if (error) {
