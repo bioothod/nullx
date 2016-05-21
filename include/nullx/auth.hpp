587fec51eeb8741bca0ea9892ea0ff955d6cbe65
Evgeniy Polyakov <zbr@ioremap.net>
2016-05-22 02:54:54 +0300

    auth: added user update, refactor processing

--- include/nullx/auth.hpp
+++ include/nullx/auth.hpp
@@ -6,6 +6,8 @@
 #include "nullx/metadata.hpp"
 
 #include <ebucket/bucket_processor.hpp>
+#include <greylock/index.hpp>
+#include <ribosome/vector_lock.hpp>
 
 #include <greylock/io.hpp>
 #include <swarm/url_query.hpp>
@@ -25,21 +27,18 @@ public:
 	{
 	}
 
-	elliptics::error_info new_user(const std::string &username, const std::string &secret, mailbox_t &mbox) {
+	elliptics::error_info new_user(mailbox_t &mbox) {
 		ebucket::bucket b;
 		auto err = m_bp.get_bucket(1024 * 1024, b);
 		if (err)
 			return err;
 
-		mbox.username = username;
-		mbox.secret = secret;
 		mbox.meta_bucket = b->name();
-		mbox.meta_index = "tags";
+		mbox.meta_index = mbox.username + ".tags";
 
 		std::stringstream buffer;
 		msgpack::pack(buffer, mbox);
 		buffer.seekg(0);
-
 
 		err = m_bp.find_bucket(m_meta_bucket, b);
 		if (err)
@@ -49,28 +48,57 @@ public:
 		elliptics::session s = b->session();
 		s.set_ioflags(DNET_IO_FLAGS_COMPARE_AND_SWAP);
 
-		auto async = s.write_data(username, data, 0);
+		auto async = s.write_data(generate_username_key(mbox.username), data, 0);
 		if (!async.is_valid()) {
-			return elliptics::create_error(-EINVAL, "auth: could not create new user: %s, invalid async, data size: %ld",
-					username.c_str(), data.size());
+			return elliptics::create_error(-EINVAL, "auth: could not write new user: %s, "
+					"invalid async, data size: %ld",
+					mbox.username.c_str(), data.size());
 		}
 
 		async.wait();
 		if (async.error()) {
 			return elliptics::create_error(async.error().code(),
 					"auth: could not write new user data, bucket: %s, username: %s, data size: %ld, error: %s",
-					m_meta_bucket.c_str(), username.c_str(), data.size(), async.error().message().c_str());
+					m_meta_bucket.c_str(), mbox.username.c_str(), data.size(), async.error().message().c_str());
 		}
-
-		generate_temporal_bits(mbox);
 
 		return elliptics::error_info();
 	}
 
-	elliptics::error_info login(const std::string &username, const std::string &secret, mailbox_t &mbox) {
+	elliptics::error_info update_user(mailbox_t &mbox) {
+		std::stringstream buffer;
+		msgpack::pack(buffer, mbox);
+		buffer.seekg(0);
+
+		ebucket::bucket b;
+		auto err = m_bp.find_bucket(m_meta_bucket, b);
+		if (err)
+			return err;
+
+		std::string data = buffer.str();
+		elliptics::session s = b->session();
+
+		auto async = s.write_data(generate_username_key(mbox.username), data, 0);
+		if (!async.is_valid()) {
+			return elliptics::create_error(-EINVAL, "auth: could not update user: %s, "
+					"invalid async, data size: %ld",
+					mbox.username.c_str(), data.size());
+		}
+
+		async.wait();
+		if (async.error()) {
+			return elliptics::create_error(async.error().code(),
+					"auth: could not update username: %s, data size: %ld, error: %s",
+					mbox.username.c_str(), data.size(), async.error().message().c_str());
+		}
+
+		return elliptics::error_info();
+	}
+
+	elliptics::error_info login(mailbox_t &mbox) {
 		greylock::eurl url;
 		url.bucket = m_meta_bucket;
-		url.key = username;
+		url.key = generate_username_key(mbox.username);
 
 		elliptics::async_read_result async = greylock::io::read_data(m_bp, url, false);
 		if (!async.is_valid()) {
@@ -93,6 +121,8 @@ public:
 					url.str().c_str(), ent.is_valid(), async.error().message().c_str());
 		}
 
+		std::string user_password = mbox.password;
+
 		const auto &file = ent.file();
 		try {
 			msgpack::unpacked result;
@@ -105,11 +135,9 @@ public:
 				url.str().c_str(), file.size(), e.what());
 		}
 
-		if (mbox.secret != secret) {
-			return elliptics::create_error(-EPERM, "auth: secret mismatch for username: %s", url.str().c_str());
+		if (mbox.password != user_password) {
+			return elliptics::create_error(-EPERM, "auth: password mismatch for username: %s", url.str().c_str());
 		}
-
-		generate_temporal_bits(mbox);
 
 		return elliptics::error_info();
 	}
@@ -119,7 +147,6 @@ public:
 		// this is needed to put ending zero-byte, otherwise rapidjson parser will explode
 		std::string data(const_cast<char *>(boost::asio::buffer_cast<const char*>(buffer)), boost::asio::buffer_size(buffer));
 
-#if 0
 		rapidjson::Document doc;
 		doc.Parse<0>(data.c_str());
 
@@ -133,13 +160,21 @@ public:
 		}
 
 		const char *username = ebucket::get_string(doc, "username");
-		const char *secret = ebucket::get_string(doc, "secret");
+		const char *password = ebucket::get_string(doc, "password");
+		const char *email = ebucket::get_string(doc, "email");
+		const char *realname = ebucket::get_string(doc, "realname", username);
 
-		if (!username || !secret) {
-			return elliptics::create_error(-EINVAL, "username and secret must be specified");
+		if (!username || !password) {
+			return elliptics::create_error(-EINVAL, "username and password must be specified");
 		}
-#endif
 
+		mbox.username.assign(username);
+		mbox.password.assign(password);
+		mbox.realname.assign(realname);
+
+		if (email)
+			mbox.email.assign(email);
+#if 0
 		swarm::url_query q(data);
 		auto username = q.item_value("username");
 		auto password = q.item_value("password");
@@ -148,7 +183,8 @@ public:
 		}
 
 		mbox.username.assign(*username);
-		mbox.secret.assign(*password);
+		mbox.password.assign(*password);
+#endif
 		return elliptics::error_info();
 	}
 
@@ -157,6 +193,12 @@ public:
 
 		rapidjson::Value username_val(mbox.username.c_str(), mbox.username.size(), reply.GetAllocator());
 		reply.AddMember("username", username_val, reply.GetAllocator());
+
+		rapidjson::Value realname_val(mbox.realname.c_str(), mbox.realname.size(), reply.GetAllocator());
+		reply.AddMember("realname", realname_val, reply.GetAllocator());
+
+		rapidjson::Value email_val(mbox.email.c_str(), mbox.email.size(), reply.GetAllocator());
+		reply.AddMember("email", email_val, reply.GetAllocator());
 
 		rapidjson::Value meta_bucket_val(mbox.meta_bucket.c_str(), mbox.meta_bucket.size(), reply.GetAllocator());
 		reply.AddMember("meta_bucket", meta_bucket_val, reply.GetAllocator());
@@ -197,7 +239,7 @@ public:
 	}
 
 	void generate_temporal_bits(mailbox_t &mbox) const {
-		std::string data = mbox.username + mbox.secret + crypto::get_random_string() + std::to_string(rand() + time(NULL));
+		std::string data = mbox.username + mbox.password + crypto::get_random_string() + std::to_string(rand() + time(NULL));
 		mbox.cookie = crypto::calc_hash<CryptoPP::Weak::MD5>(data.data(), data.size());
 		mbox.max_age = 600;
 		mbox.expires_at = std::chrono::system_clock::now() + std::chrono::seconds(mbox.max_age);
@@ -207,36 +249,45 @@ private:
 	ebucket::bucket_processor &m_bp;
 	std::string m_meta_bucket;
 	std::string m_domain;
+
+	std::string generate_username_key(const std::string &key) {
+		std::string prefix = "u";
+
+		char tmp[prefix.size() + 1 + key.size() + 1];
+		int sz = snprintf(tmp, sizeof(tmp), "%s.%s", prefix.c_str(), key.c_str());
+		tmp[prefix.size()] = '\0';
+
+		return std::string(tmp, sz);
+	}
 };
 
 template <typename Server, typename Stream, bool login>
 class on_auth_base : public thevoid::simple_request_stream<Server>, public std::enable_shared_from_this<Stream> {
 public:
-	void on_request(const thevoid::http_request &req, const boost::asio::const_buffer &buffer) {
-		(void) req;
+	virtual elliptics::error_info process(auth &a, mailbox_t &mbox) = 0;
 
+	void on_request(const thevoid::http_request &req, const boost::asio::const_buffer &buffer) {
 		auth a(*this->server()->bucket_processor(), this->server()->meta_bucket(), this->server()->domain());
 		mailbox_t mbox;
 
 		auto err = a.parse_request(req, buffer, mbox);
 		if (err) {
-			NLOG_ERROR("login: failed to parse request: %s [%d]", err.message().c_str(), err.code());
+			NLOG_ERROR("auth: url: %s, failed to parse request: %s [%d]",
+					req.url().to_human_readable().c_str(),
+					err.message().c_str(), err.code());
 			this->send_reply(swarm::http_response::bad_request);
 			return;
 		}
 
-		if (login) {
-			err = a.login(mbox.username, mbox.secret, mbox);
-		} else {
-			err = a.new_user(mbox.username, mbox.secret, mbox);
-		}
-
+		err = process(a, mbox);
 		if (!err) {
+			a.generate_temporal_bits(mbox);
 			err = this->server()->store_auth(mbox);
 		}
 
 		if (err) {
-			NLOG_ERROR("login: failed to create new user '%s': %s [%d]",
+			NLOG_ERROR("auth: url: %s, failed to process user: %s, error: %s [%d]",
+					req.url().to_human_readable().c_str(),
 					mbox.username.c_str(), err.message().c_str(), err.code());
 
 			int code = err.code();
@@ -244,7 +295,7 @@ public:
 			int status = swarm::http_response::service_unavailable;
 			if (err.code() == -EPERM) {
 				status = swarm::http_response::unauthorized;
-				message = "user " + mbox.username + ": invalid secret";
+				message = "user " + mbox.username + ": invalid password";
 			}
 			if (err.code() == -EBADFD) {
 				code = -EEXIST;
@@ -284,15 +335,60 @@ private:
 };
 
 template <typename Server>
-class on_login : public on_auth_base<Server, on_login<Server>, true>
+class on_user_login : public on_auth_base<Server, on_user_login<Server>, true>
 {
 public:
+	elliptics::error_info process(auth &a, mailbox_t &mbox) {
+		auto err = a.login(mbox);
+		if (err)
+			return err;
+
+		NLOG_INFO("auth: login: welcome: username: %s, realname: %s, email: %s, meta_bucket: %s, meta_index: %s",
+				mbox.username.c_str(), mbox.realname.c_str(), mbox.email.c_str(),
+				mbox.meta_bucket.c_str(), mbox.meta_index.c_str());
+		return err;
+	}
 };
 
 template <typename Server>
-class on_signup : public on_auth_base<Server, on_signup<Server>, false>
+class on_user_signup : public on_auth_base<Server, on_user_signup<Server>, false>
 {
 public:
+	elliptics::error_info process(auth &a, mailbox_t &mbox) {
+		auto err = a.new_user(mbox);
+		if (err)
+			return err;
+
+		greylock::eurl idx;
+		idx.bucket = mbox.meta_bucket;
+		idx.key = mbox.meta_index;
+
+		try {
+			ribosome::locker<Server> l(this->server(), idx.str());
+			std::unique_lock<ribosome::locker<Server>> lk(l);
+
+			greylock::read_write_index index(*this->server()->bucket_processor(), idx);
+		} catch (const elliptics::error &e) {
+			return elliptics::create_error(e.error_code(),
+					"auth: could not create meta index: %s, username: %s, error: %s",
+					idx.str().c_str(), mbox.username.c_str(), e.what());
+		} catch (const std::exception &e) {
+			return elliptics::create_error(-EINVAL,
+					"auth: could not create meta index: %s, username: %s, error: %s",
+					idx.str().c_str(), mbox.username.c_str(), e.what());
+		}
+
+		return elliptics::error_info();
+	}
+};
+
+template <typename Server>
+class on_user_update : public on_auth_base<Server, on_user_update<Server>, false>
+{
+public:
+	elliptics::error_info process(auth &a, mailbox_t &mbox) {
+		return a.update_user(mbox);
+	}
 };
 
 
