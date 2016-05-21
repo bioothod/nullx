c46b87bcb818b3735210b44b81ac80adbec00bfe
Evgeniy Polyakov <zbr@ioremap.net>
2016-05-22 02:55:32 +0300

    metadata: store realname and email as well

--- include/nullx/metadata.hpp
+++ include/nullx/metadata.hpp
@@ -6,11 +6,13 @@ namespace ioremap { namespace nullx {
 
 struct mailbox_t {
 	std::string		username;
-	std::string		secret;
+	std::string		password;
+	std::string		realname;
+	std::string		email;
 	std::string		meta_bucket;
 	std::string		meta_index;
 
-	MSGPACK_DEFINE(username, secret, meta_bucket, meta_index);
+	MSGPACK_DEFINE(username, password, realname, email, meta_bucket, meta_index);
 
 	std::string		cookie;
 	std::chrono::system_clock::time_point	expires_at;
