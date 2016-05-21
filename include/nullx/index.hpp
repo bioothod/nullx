ea0bd5b1862936618695ab051a213aa2087435fc
Evgeniy Polyakov <zbr@ioremap.net>
2016-05-22 02:56:55 +0300

    index: added tag index listing

--- include/nullx/index.hpp
+++ include/nullx/index.hpp
@@ -33,7 +33,7 @@ public:
 		doc.Parse<0>(data.c_str());
 
 		if (doc.HasParseError()) {
-			NLOG_ERROR("index: on_request: url: %s: could not parse document: %s, error offset: %zd, data: %s", 
+			NLOG_ERROR("index: on_request: url: %s: could not parse document: %s, error offset: %zd, data: %s",
 					req.url().to_human_readable().c_str(),
 					doc.GetParseError(), doc.GetErrorOffset(), data.c_str());
 			this->send_reply(swarm::http_response::bad_request);
@@ -41,7 +41,7 @@ public:
 		}
 
 		if (!doc.IsObject()) {
-			NLOG_ERROR("index: on_request: url: %s: document must be object, its type: %d", 
+			NLOG_ERROR("index: on_request: url: %s: document must be object, its type: %d",
 					req.url().to_human_readable().c_str(), doc.GetType());
 			this->send_reply(swarm::http_response::bad_request);
 			return;
@@ -49,7 +49,7 @@ public:
 
 		const rapidjson::Value &files = ebucket::get_array(doc, "files");
 		if (!files.IsArray()) {
-			NLOG_ERROR("index: on_request: url: %s: document must contain 'files' array", 
+			NLOG_ERROR("index: on_request: url: %s: document must contain 'files' array",
 					req.url().to_human_readable().c_str());
 			this->send_reply(swarm::http_response::bad_request);
 			return;
@@ -190,15 +190,24 @@ private:
 			obj.url.bucket = bucket;
 			obj.url.key = key;
 
-			ribosome::locker<Server> l(this->server(), iname);
+			greylock::eurl idx;
+			idx.bucket = bucket;
+			idx.key = iname;
+
+			ribosome::locker<Server> l(this->server(), idx.str());
 			std::unique_lock<ribosome::locker<Server>> lk(l);
 
-			auto err = insert_index(iname, obj);
+			auto err = insert_index(idx, obj);
 			if (err)
 				return err;
 		}
 
-		ribosome::locker<Server> l(this->server(), m_mbox.meta_index);
+
+		greylock::eurl idx;
+		idx.bucket = m_mbox.meta_bucket;
+		idx.key = m_mbox.meta_index;
+
+		ribosome::locker<Server> l(this->server(), idx.str());
 		std::unique_lock<ribosome::locker<Server>> lk(l);
 
 		for (const auto &iname: indexes) {
@@ -210,7 +219,7 @@ private:
 			obj.url.bucket = m_mbox.meta_bucket;
 			obj.url.key = iname;
 
-			auto err = insert_index(m_mbox.meta_index, obj);
+			auto err = insert_index(idx, obj);
 			if (err)
 				return err;
 		}
@@ -218,35 +227,30 @@ private:
 		return elliptics::error_info();
 	}
 
-	elliptics::error_info insert_index(const std::string &iname, const greylock::key &obj) {
+	elliptics::error_info insert_index(const greylock::eurl &idx, const greylock::key &obj) {
 		ribosome::timer tm;
 
 		NLOG_NOTICE("insert_index: updating index: %s, obj: %s",
-				iname.c_str(), obj.str().c_str());
+				idx.str().c_str(), obj.str().c_str());
 		try {
-			greylock::eurl idx;
-
-			idx.bucket = m_mbox.meta_bucket;
-			idx.key = iname;
-
 			greylock::read_write_index index(*this->server()->bucket_processor(), idx);
 
 			NLOG_NOTICE("insert_index: inserting index: %s, obj: %s",
-					iname.c_str(), obj.str().c_str());
+					idx.str().c_str(), obj.str().c_str());
 			elliptics::error_info err = index.insert(obj);
 			if (err) {
 				return elliptics::create_error(err.code(), "insert_index: "
 						"index: %s, obj: %s: could not insert new key: %s [%d]",
-					iname.c_str(), obj.str().c_str(), err.message().c_str(), err.code());
+							idx.str().c_str(), obj.str().c_str(), err.message().c_str(), err.code());
 			}
 		} catch (const std::exception &e) {
 			return elliptics::create_error(-EINVAL, "insert_index: "
 					"index: %s, obj: %s: could not insert new key, exception: %s",
-				iname.c_str(), obj.str().c_str(), e.what());
+						idx.str().c_str(), obj.str().c_str(), e.what());
 		}
 
 		NLOG_INFO("insert_index: index: %s, obj: %s, elapsed-time: %ld ms",
-				iname.c_str(), obj.str().c_str(), tm.restart());
+				idx.str().c_str(), obj.str().c_str(), tm.restart());
 		return elliptics::error_info();
 	}
 
@@ -254,6 +258,190 @@ private:
 
 template <typename Server>
 class on_index : public on_index_base<Server, on_index<Server>>
+{
+public:
+};
+
+template <typename Server, typename Stream>
+class on_list_base: public thevoid::simple_request_stream<Server>, public std::enable_shared_from_this<Stream>
+{
+public:
+	virtual void on_request(const thevoid::http_request &req, const boost::asio::const_buffer &buffer) {
+		if (!this->server()->check_cookie(req, m_mbox)) {
+			NLOG_ERROR("list: on_request: url: %s: invalid cookie, redirecting to login page",
+					req.url().to_human_readable().c_str());
+			thevoid::http_response reply;
+			reply.headers().set("Access-Control-Allow-Origin", "*");
+			reply.set_code(swarm::http_response::forbidden);
+			reply.headers().set_content_length(0);
+			this->send_reply(std::move(reply));
+			return;
+		}
+
+		NLOG_INFO("list: on_request: url: %s: auth succeeded: username: %s, meta_bucket: %s, meta_index: %s",
+				req.url().to_human_readable().c_str(),
+				m_mbox.username.c_str(), m_mbox.meta_bucket.c_str(), m_mbox.meta_index.c_str());
+
+		std::string data(const_cast<char *>(boost::asio::buffer_cast<const char*>(buffer)), boost::asio::buffer_size(buffer));
+		rapidjson::Document doc;
+		doc.Parse<0>(data.c_str());
+
+		if (doc.HasParseError()) {
+			NLOG_ERROR("list: on_request: url: %s: could not parse document: %s, error offset: %zd, data: %s",
+					req.url().to_human_readable().c_str(),
+					doc.GetParseError(), doc.GetErrorOffset(), data.c_str());
+			this->send_reply(swarm::http_response::bad_request);
+			return;
+		}
+
+		if (!doc.IsObject()) {
+			NLOG_ERROR("list: on_request: url: %s: document must be object, its type: %d",
+					req.url().to_human_readable().c_str(), doc.GetType());
+			this->send_reply(swarm::http_response::bad_request);
+			return;
+		}
+
+		const rapidjson::Value &tags = ebucket::get_array(doc, "tags");
+		if (!tags.IsArray()) {
+			NLOG_ERROR("index: on_request: url: %s: document must contain 'tags' array",
+					req.url().to_human_readable().c_str());
+			this->send_reply(swarm::http_response::bad_request);
+			return;
+		}
+
+		nullx::JsonValue value;
+		rapidjson::Value tvals;
+		tvals.SetArray();
+
+		std::vector<greylock::eurl> tnames;
+		for (auto it = tags.Begin(), end = tags.End(); it != end; ++it) {
+			if (!it->IsObject()) {
+				NLOG_ERROR("index: on_request: url: %s: tags array must contain objects",
+						req.url().to_human_readable().c_str());
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+			const char *bucket = ebucket::get_string(*it, "bucket");
+			const char *key = ebucket::get_string(*it, "key");
+			if (!bucket || !key) {
+				NLOG_ERROR("index: on_request: url: %s: tags array element must contain bucket and key strings, "
+						"bucket: %s, key: %s",
+						req.url().to_human_readable().c_str(), bucket, key);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+			greylock::eurl url;
+			url.bucket = bucket;
+			url.key = key;
+
+			tnames.emplace_back(url);
+		}
+
+		if (tnames.empty()) {
+			greylock::eurl meta;
+			meta.bucket = m_mbox.meta_bucket;
+			meta.key = m_mbox.meta_index;
+			tnames.emplace_back(meta);
+		}
+
+		read_indexes(tvals, tnames, value.GetAllocator());
+
+		value.AddMember("tags", tvals, value.GetAllocator());
+
+		std::string reply_data = value.ToString();
+
+		thevoid::http_response reply;
+		reply.set_code(swarm::http_response::ok);
+		reply.headers().set("Access-Control-Allow-Origin", "*");
+		reply.headers().set_content_type("text/json; charset=utf-8");
+		reply.headers().set_content_length(reply_data.size());
+
+		this->send_reply(std::move(reply), std::move(reply_data));
+	}
+
+private:
+	mailbox_t m_mbox;
+
+	template <typename Allocator>
+	void read_indexes(rapidjson::Value &tvals, const std::vector<greylock::eurl> &tnames, Allocator &allocator) {
+		for (const auto &tag: tnames) {
+			rapidjson::Value tval(rapidjson::kObjectType);
+
+			rapidjson::Value tname(rapidjson::kObjectType);
+			pack_eurl(tname, tag, allocator);
+			tval.AddMember("tag", tname, allocator);
+
+			try {
+				rapidjson::Value kvals(rapidjson::kArrayType);
+
+				ribosome::locker<Server> l(this->server(), tag.str());
+				std::unique_lock<ribosome::locker<Server>> lk(l);
+
+				greylock::read_only_index index(*this->server()->bucket_processor(), tag);
+
+				for (auto it = index.begin(), end = index.end(); it != end; ++it) {
+					rapidjson::Value key(rapidjson::kObjectType);
+
+					pack_key(key, *it, allocator);
+					kvals.PushBack(key, allocator);
+				}
+
+				tval.AddMember("keys", kvals, allocator);
+			} catch (const elliptics::error &e) {
+				NLOG_ERROR("list: read_indexes: tag: %s, elliptics exception: %s [%d]",
+						tag.str().c_str(), e.what(), e.error_code());
+				add_error(tval, e.error_code(), e.what(), allocator);
+			} catch (const std::exception &e) {
+				NLOG_ERROR("list: read_indexes: tag: %s, exception: %s", tag.str().c_str(), e.what());
+				add_error(tval, -EINVAL, e.what(), allocator);
+			}
+
+			tvals.PushBack(tval, allocator);
+		}
+	}
+
+	template <typename Allocator>
+	void pack_eurl(rapidjson::Value &e, const greylock::eurl &url, Allocator &allocator) {
+		rapidjson::Value bval(url.bucket.c_str(), url.bucket.size(), allocator);
+		e.AddMember("bucket", bval, allocator);
+
+		rapidjson::Value kval(url.key.c_str(), url.key.size(), allocator);
+		e.AddMember("key", kval, allocator);
+	}
+
+	template <typename Allocator>
+	void pack_key(rapidjson::Value &e, const greylock::key &key, Allocator &allocator) {
+		pack_eurl(e, key.url, allocator);
+
+		rapidjson::Value ival(key.id.c_str(), key.id.size(), allocator);
+		e.AddMember("id", ival, allocator);
+
+		rapidjson::Value ts(rapidjson::kObjectType);
+		long tsec, tnsec;
+		key.get_timestamp(tsec, tnsec);
+		ts.AddMember("tsec", tsec, allocator);
+		ts.AddMember("tnsec", tnsec, allocator);
+
+		e.AddMember("timestamp", ts, allocator);
+	}
+
+	template <typename Allocator>
+	void add_error(rapidjson::Value &val, int code, const char *message, Allocator &allocator) {
+		rapidjson::Value e(rapidjson::kObjectType);
+
+		e.AddMember("code", code, allocator);
+
+		rapidjson::Value m(message, strlen(message), allocator);
+		e.AddMember("message", m, allocator);
+
+		val.AddMember("error", e, allocator);
+	}
+};
+
+template <typename Server>
+class on_list : public on_list_base<Server, on_list<Server>>
 {
 public:
 };
